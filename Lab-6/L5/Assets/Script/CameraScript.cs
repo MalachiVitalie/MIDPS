b002d5b93548a606e5a8dae538cb89cd2a7fd63a
MalachiVitalie <myname.vetalik@gmail.com>
2016-05-29 21:24:09 -0800

    lab nr.6

---
+++ Lab-6/L5/Assets/Script/CameraScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+	public Transform lookAt;
+	private Vector3 offset = new Vector3(0,0,-14f);
+ 	private void Start () {
+	}
+	
+	// Update is called once per frame
+	private void LateUpdate () {
+		transform.position = lookAt.transform.position + offset;
+	}
+}
---
+++ Lab-6/L5/Assets/Script/LevelManager.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+	public static LevelManager Instance{ set; get;}
+
+	private int hitPoint = 3;
+	private int scoree = 0;
+
+	public Text scoreText;
+	public Text hitpointText;
+
+	public Transform spownPosition;
+	public Transform playerTransform;
+
+	private void Awake(){
+		Instance = this;
+		scoreText.text = "Curent Score: " + scoree.ToString();
+		hitpointText.text = "Hitpoint: " + hitPoint.ToString();
+	}
+
+	private void Update(){
+		
+		if (playerTransform.position.y < -8) {
+			playerTransform.position = spownPosition.position;
+			hitPoint--;
+			hitpointText.text = "Hitpoint: " + hitPoint.ToString();
+			if(hitPoint < 0){
+				Application.LoadLevel("Menu"); 
+			}
+		}
+		
+	}
+	
+	public void Win(){
+		if(scoree > PlayerPrefs.GetInt("PlayerScore", scoree))
+				PlayerPrefs.SetInt ("PlayerScore", scoree);
+		else PlayerPrefs.SetInt ("PlayerScore", scoree);
+		PlayerPrefs.Save();
+
+		Application.LoadLevel ("Menu");
+	}
+
+	public void CollectCoin(){
+		scoree++;
+		scoreText.text = "Curent Score: " + scoree.ToString();
+	}
+}
